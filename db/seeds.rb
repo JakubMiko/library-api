46a78b36d898dd04fc79e811b95532b27e938885
Jakub Mikołajczyk <kubamiko11@gmail.com>
2026-02-04 00:45:37 +0100

    Create seed data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,25 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# frozen_string_literal: true
+
+readers = 5.times.map do
+  Reader.create!(
+    card_number: Faker::Number.unique.number(digits: 6).to_s,
+    name: Faker::Name.name,
+    email: Faker::Internet.unique.email
+  )
+end
+
+books = 10.times.map do
+  Book.create!(
+    serial_number: Faker::Number.unique.number(digits: 6).to_s,
+    title: Faker::Book.title,
+    author: Faker::Book.author
+  )
+end
+
+Borrowing.create!(book: books[0], reader: readers[0], borrowed_at: 5.days.ago, due_date: 25.days.from_now)
+Borrowing.create!(book: books[1], reader: readers[1], borrowed_at: 25.days.ago, due_date: 5.days.from_now)
+
+Borrowing.create!(book: books[2], reader: readers[0], borrowed_at: 45.days.ago, due_date: 15.days.ago, returned_at: 10.days.ago)
+Borrowing.create!(book: books[3], reader: readers[1], borrowed_at: 40.days.ago, due_date: 10.days.ago, returned_at: 5.days.ago)
+
+puts "Created #{Reader.count} readers, #{Book.count} books and #{Borrowing.count} borrowings"
